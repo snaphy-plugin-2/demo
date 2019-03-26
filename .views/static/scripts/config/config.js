33829d45c6efceb981bcabade41526f724c4aa9e
rishi <erishiraj@gmail.com>
2019-03-26 10:23:53 +0530

    Adding disabled property in input

--- .views/static/scripts/config/config.js
+++ .views/static/scripts/config/config.js
@@ -8,18 +8,16 @@ angular.module($snaphy.getModuleName())
 /**
  Defigning templated for angular-formly.
  */
-.run(['formlyConfig', '$rootScope',  function(formlyConfig, $rootScope) {
+.run(['formlyConfig', "$rootScope", "$timeout",  function(formlyConfig, $rootScope, $timeout) {
     formlyConfig.setType({
         name: 'input',
-        template: '<div ng-if="options.templateOptions.display !== false" >'+
-            '<div  ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}" >' +
+        template: '<div  ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}" >' +
             '<div  ng-class="[options.templateOptions.colSize, options.templateOptions.color]">' +
             '<div class="form-material" ng-class="options.templateOptions.color">' +
-            '<input  ng-disabled="{{options.templateOptions.disabled}}" class="form-control input-box" type="{{options.templateOptions.type}}"  ng-class="options.templateOptions.class" name="{{options.templateOptions.id}}" id="{{options.templateOptions.id}}" ng-model="model[options.key]">' +
+            '<input ng-change="onDataChange()" ng-disabled="options.templateOptions.disabled" class="form-control input-box" type="{{options.templateOptions.type}}"  ng-class="options.templateOptions.class" name="{{options.templateOptions.id}}" id="{{options.templateOptions.id}}" ng-model="model[options.key]">' +
             '<label for="{{options.templateOptions.id}}">{{options.templateOptions.label}}</label>' +
             '</div>' +
             '</div>' +
-            '</div>'+
             '</div>',
 
         link: function(scope) {
@@ -31,35 +29,59 @@ angular.module($snaphy.getModuleName())
                 } //if
 
 
-                if(!scope.model[scope.options.key]){
-                    //Check if default value is given..
-                    if(scope.to.default){
-                        scope.model[scope.options.key] = scope.to.default;
+                /**
+                 * Will run whenever data is getting changed.
+                 */
+                scope.onDataChange = function(){
+                    if( scope.options.templateOptions.onChange){
+                        //BroadCast the listener if any present..
+                        $rootScope.$broadcast(scope.options.templateOptions.onChange, {
+                            data: scope.model[scope.options.key],
+                            model: scope.model,
+                            key: scope.options.key
+                        });
                     }
-                }
+                }; 
 
-                if(scope.to.listen){
-                    //Args will have certain values..
-                    //{ to:{}, data: "" } to is templateOptions object.., data is value
-                    var listenEvent = $rootScope.$on(scope.to.listen, function(event, args){
-                        console.log(args);
 
-                        if(args.to){
-                            //Copy args template options to old template options..
-                            angular.copy(args.to, scope.to);
-                        }
-
-                        //Add value if present..
-                        if(args.data){
+                //Listen to listener if present ..
+                if(scope.options.templateOptions.loadData){
+                    //Will add model data in args.data property..
+                    var loadListener = $rootScope.$on(scope.options.templateOptions.loadData, function(event, args){
+                        console.log("Listening for events", args.data);
+                        //Unsubscribe
+                        //loadListener();
+                        $timeout(function(){
                             scope.model[scope.options.key] = args.data;
-                        }
+                        });
                     });
 
                     ///Destroy event on page change..
                     scope.$on('$destroy', function() {
-                        if(listenEvent){
-                            listenEvent();
-                        }
+                        //Unsubscribe
+                        loadListener();
+                    });
+                }
+
+                //Listen to listener if present ..
+                if(scope.options.templateOptions.onPropertyChange){
+                    //Will add model data in args.data property..
+                    var loadListener = $rootScope.$on(scope.options.templateOptions.onPropertyChange, function(event, args){     
+                        $timeout(function(){
+                            if(args.properties){
+                                for(var key in args.properties){
+                                    if(args.properties.hasOwnProperty(key)){
+                                        scope.options.templateOptions[key] = args.properties[key]
+                                    }
+                                }
+                            }
+                        });
+                    });
+
+                    ///Destroy event on page change..
+                    scope.$on('$destroy', function() {
+                        //Unsubscribe
+                        loadListener();
                     });
                 }
 
@@ -70,13 +92,11 @@ angular.module($snaphy.getModuleName())
 
     formlyConfig.setType({
         name: 'textarea',
-        template:'<div ng-if="options.templateOptions.display !== false" >'+ 
-            '<div ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}">' +
+        template: '<div ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}">' +
             '<div ng-class="options.templateOptions.colSize">' +
             '<div class="form-material" ng-class="options.templateOptions.color">' +
             '<textarea ng-disabled="{{options.templateOptions.disabled}}" type="{{options.templateOptions.type}}" name="{{options.templateOptions.id}}" id="{{options.templateOptions.id}}" ng-class="options.templateOptions.class" class="form-control input-box" ng-model="model[options.key]" rows="{{options.templateOptions.row}}"></textarea>' +
             '<label for="{{options.templateOptions.id}}">{{options.templateOptions.label}}</label>' +
-            '</div>' +
             '</div>' +
             '</div>' +
             '</div>',
@@ -105,8 +125,7 @@ angular.module($snaphy.getModuleName())
                "filter":{} //filter to fetch the data..
            }
         * */
-        template: '<div ng-if="options.templateOptions.display !== false" >'+ 
-            '<div ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}">' +
+        template: '<div ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}">' +
             '<div ng-class="options.templateOptions.colSize">' +
             '<div class="form-material" ng-class="options.templateOptions.color">' +
             '<select ng-disabled="{{options.templateOptions.disabled}}" type="{{options.templateOptions.type}}" name="{{options.templateOptions.id}}" ng-class="options.templateOptions.class" id="{{options.templateOptions.id}}" ng-change="convertToString(model[options.key])" ng-model="model[options.key]" class="form-control input-box"  size="{{options.templateOptions.size}}">' +
@@ -114,7 +133,6 @@ angular.module($snaphy.getModuleName())
             '<option value="{{option.id}}" ng-repeat="option in options.templateOptions.options">{{option.name}}</option>' +
             '</select>' +
             '<label for="{{options.templateOptions.id}}">{{options.templateOptions.label}}</label>' +
-            '</div>' +
             '</div>' +
             '</div>' +
             '</div>',
@@ -192,17 +210,14 @@ angular.module($snaphy.getModuleName())
     //For selecting string only .. previous for selecting object..
     formlyConfig.setType({
         name: 'selectString',
-        template:
-            '<div ng-if="options.templateOptions.display !== false" >'+  
-            '<div ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}">' +
+        template: '<div ng-class="{\'form-group\': !options.templateOptions.inline, \'inline-elements\': options.templateOptions.inline}">' +
             '<div ng-class="options.templateOptions.colSize">' +
             '<div class="form-material" ng-class="options.templateOptions.color">' +
-            '<select ng-disabled="{{options.templateOptions.disabled}}" type="{{options.templateOptions.type}}" name="{{options.templateOptions.id}}" ng-class="options.templateOptions.class" id="{{options.templateOptions.id}}"   ng-model="model[options.key]" class="form-control input-box"  size="{{options.templateOptions.size}}">' +
+            '<select ng-change="OnChange" ng-disabled="{{options.templateOptions.disabled}}" type="{{options.templateOptions.type}}" name="{{options.templateOptions.id}}" ng-class="options.templateOptions.class" id="{{options.templateOptions.id}}"   ng-model="model[options.key]" class="form-control input-box"  size="{{options.templateOptions.size}}">' +
             '<option value=""></option>' +
             '<option value="{{option}}" ng-repeat="option in options.templateOptions.options">{{option | uppercase}}</option>' +
             '</select>' +
             '<label for="{{options.templateOptions.id}}">{{options.templateOptions.label}}</label>' +
-            '</div>' +
             '</div>' +
             '</div>' +
             '</div>',
@@ -215,8 +230,6 @@ angular.module($snaphy.getModuleName())
             if ($scope.options.templateOptions.colSize === undefined) {
                 $scope.options.templateOptions.colSize = "col-sm-12";
             }
-
-            
 
 
             if ($scope.options.templateOptions.get !== undefined) {
@@ -235,32 +248,31 @@ angular.module($snaphy.getModuleName())
                 });
             }
 
+            $scope.onChange = function(){
+                
+            };
+
+            
             $scope.$watch("model[options.key]",
                 function() {
                     if($scope.model){
                         if($scope.model[$scope.options.key] !== undefined){
                             //console.log($scope.model);
                             $scope.model[$scope.options.key] = $scope.model[$scope.options.key].toString();
-                        }else{
-                            if($scope.to.default){
-                                if(!$scope.model[$scope.options.key]){
-                                    $scope.model[$scope.options.key] = $scope.to.default; 
-                                }
+                            
+                            if($scope.to.onSelect){
+                                //Broadcast Event if present..
+                                $rootScope.$broadcast($scope.to.onSelect, {
+                                    data: $scope.model[$scope.options.key],
+                                    model: $scope.model,
+                                    key: $scope.options.key
+                                });
                             }
-                        }
 
-                        if($scope.to.onSelect){
-                            //Broadcast Event if present..
-                            $rootScope.$broadcast($scope.to.onSelect, {
-                                data: $scope.model[$scope.options.key],
-                                model: $scope.model,
-                                key: $scope.options.key
-                            });
                         }
                     }
                 }
             );
-
         }]
 
     });
